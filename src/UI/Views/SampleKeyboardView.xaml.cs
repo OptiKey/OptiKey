7383a79acb7e1d00e68718222b1939193d6be7fb
juliussweetland <juliussweetland@gmail.com>
2014-07-18 18:42:49 +0100

    Added new SampleKeyboardView

---
+++ src/UI/Views/SampleKeyboardView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace JuliusSweetland.ETTA.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for SampleKeyboardView.xaml
+    /// </summary>
+    public partial class SampleKeyboardView : UserControl
+    {
+        public SampleKeyboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/UI/Windows/SampleKeyboardWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace JuliusSweetland.ETTA.UI.Windows
+{
+    /// <summary>
+    /// Interaction logic for SampleKeyboardWindow.xaml
+    /// </summary>
+    public partial class SampleKeyboardWindow : Window
+    {
+        public SampleKeyboardWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
