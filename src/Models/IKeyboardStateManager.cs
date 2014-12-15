a682da8a1202e306406ead9c5db81cd1ff8aa544
Julius <juliussweetland@gmail.com>
2014-12-15 20:20:36 +0000

    Update Notes.txt

--- src/Models/IKeyboardStateManager.cs
+++ src/Models/IKeyboardStateManager.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using JuliusSweetland.ETTA.Enums;
-using JuliusSweetland.ETTA.UI.ViewModels.Keyboards;
 
 namespace JuliusSweetland.ETTA.Models
 {
@@ -12,6 +11,5 @@ namespace JuliusSweetland.ETTA.Models
         List<string> Suggestions { get; set; }
         int SuggestionsPage { get; }
         int SuggestionsPerPage { get; }
-        IKeyboard Keyboard { set; }
     }
 }
