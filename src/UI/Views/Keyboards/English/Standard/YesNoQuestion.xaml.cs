5d08af7ed6bed8f59d7d8846ca90f93a8f975be2
Julius <juliussweetland@gmail.com>
2014-12-29 11:55:18 +0000

    Minor additions.

---
+++ src/UI/Views/Keyboards/English/Standard/YesNoQuestion.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace JuliusSweetland.ETTA.UI.Views.Keyboards.English.Standard
+{
+    /// <summary>
+    /// Interaction logic for YesNoQuestion.xaml
+    /// </summary>
+    public partial class YesNoQuestion : UserControl
+    {
+        public YesNoQuestion()
+        {
+            InitializeComponent();
+        }
+    }
+}
