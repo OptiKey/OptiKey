077da3f6e33a8802c3edb22235f5c467b20196f9
Julius <juliussweetland@gmail.com>
2014-12-29 22:06:43 +0000

    Adding missing file.

---
+++ src/Models/CapturingStateManager.cs
@@ -0,0 +1,36 @@
+﻿using JuliusSweetland.ETTA.Properties;
+using JuliusSweetland.ETTA.Services;
+using log4net;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace JuliusSweetland.ETTA.Models
+{
+    public class CapturingStateManager : BindableBase, ICapturingStateManager
+    {
+        private readonly static ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly IAudioService audioService;
+
+        public CapturingStateManager(IAudioService audioService)
+        {
+            this.audioService = audioService;
+        }
+
+        private bool capturingMultiKeySelection;
+        public bool CapturingMultiKeySelection
+        {
+            get { return capturingMultiKeySelection; }
+            set
+            {
+                if (SetProperty(ref capturingMultiKeySelection, value))
+                {
+                    Log.Debug(string.Format("CapturingMultiKeySelection changed to {0}", value));
+
+                    audioService.PlaySound(value
+                        ? Settings.Default.MultiKeySelectionCaptureStartSoundFile
+                        : Settings.Default.MultiKeySelectionCaptureEndSoundFile);
+                }
+            }
+        }
+    }
+}
