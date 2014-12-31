fae2842d4fe23641545fb739d233df8059bbe842
Julius <juliussweetland@gmail.com>
2014-12-31 16:57:58 +0000

    Fleshing out the management view and view model

---
+++ src/UI/ViewModels/ManagementViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+
+namespace JuliusSweetland.ETTA.UI.ViewModels
+{
+    public class ManagementViewModel : BindableBase
+    {
+
+    }
+}
---
+++ src/UI/Views/ManagementView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JuliusSweetland.ETTA.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for ManagementView.xaml
+    /// </summary>
+    public partial class ManagementView : UserControl
+    {
+        public ManagementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
