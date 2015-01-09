6fe4d28d4c2671005a63bb4c6f4c3ccc534c6a7c
Julius <juliussweetland@gmail.com>
2015-01-09 14:38:12 +0000

    Create VisualsViewModel.cs

---
+++ src/UI/ViewModels/Management/VisualsViewModel.cs
@@ -0,0 +1,59 @@
+using System.Windows;
+using JuliusSweetland.ETTA.Properties;
+using JuliusSweetland.ETTA.Services;
+using log4net;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Interactivity.InteractionRequest;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace JuliusSweetland.ETTA.UI.ViewModels.Management
+{
+    public class VisualsViewModel : BindableBase
+    {
+        #region Private Member Vars
+
+        private readonly static ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        #endregion
+        
+        #region Ctor
+
+        public VisualsViewModel()
+        {
+            LoadSettings();
+        }
+        
+        #endregion
+        
+        #region Properties
+        
+        public bool ChangesRequireRestart
+        {
+            return false;
+            
+            //Settings.Default.CaptureTriggerSource != CaptureTriggerSource
+            //  || Settings.Default.CaptureTriggerKeyboardSignal != CaptureTriggerKeyboardSignal.ToString()
+            //  || Settings.Default.CaptureCoordinatesSource != CaptureCoordinatesSource
+            //  || Settings.Default.CaptureMouseCoordinatesOnIntervalInMilliseconds != CaptureMouseCoordinatesOnIntervalInMilliseconds
+            //  || Settings.Default.CaptureCoordinatesTimeoutInMilliseconds != CaptureCoordinatesTimeoutInMilliseconds;
+        }
+        
+        #endregion
+        
+        #region Methods
+
+        private void LoadSettings()
+        {
+            //Debug = Settings.Default.Debug;
+        }
+
+        private void SaveSettings()
+        {
+            //Settings.Default.Debug = Debug;
+            
+            //Settings.Default.Save();
+        }
+
+        #endregion
+    }
+}
