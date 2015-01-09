e6540400f3bb1a53d772720d6ca99d466e93d32d
Julius <juliussweetland@gmail.com>
2015-01-09 14:55:08 +0000

    Create DictionaryView.xaml.cs

---
+++ src/UI/Views/Management/DictionaryView.xaml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JuliusSweetland.ETTA.UI.Views.Management
+{
+    /// <summary>
+    /// Interaction logic for DictionaryView.xaml
+    /// </summary>
+    public partial class DictionaryView : UserControl
+    {
+        public DictionaryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
