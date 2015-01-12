4df7c1d3c0cfb03ce5eee8ab441079bb1b4b6398
Julius <juliussweetland@gmail.com>
2015-01-12 13:51:39 +0000

    Update PointingAndSelectingViewModel.cs

--- src/UI/ViewModels/Management/PointingAndSelectingViewModel.cs
+++ src/UI/ViewModels/Management/PointingAndSelectingViewModel.cs
@@ -22,7 +22,7 @@ namespace JuliusSweetland.ETTA.UI.ViewModels.Management
         
         #region Properties
 
-        public List<KeyValuePair<string, string>> PointsSources
+        public List<KeyValuePair<string, PointsSources>> PointsSources
         {
             get
             {
@@ -35,11 +35,111 @@ namespace JuliusSweetland.ETTA.UI.ViewModels.Management
             }
         }
         
+        public List<KeyValuePair<string, TriggerSources>> TriggerSources
+        {
+            get
+            {
+                return new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("Fixations", TriggerSources.Fixations),
+                    new KeyValuePair<string, string>("Keyboard Key", TriggerSources.KeyboardKeyDownsUps),
+                    new KeyValuePair<string, string>("Mouse Button", TriggerSources.MouseButtonDownUps)
+                };
+            }
+        }
+        
+        public List<Keys> Keys
+        {
+            get { return Enum.GetValues(typeof(Keys)).Cast<Keys>().ToList(); }
+        }
+        
+        public List<MouseButtons> MouseButtons
+        {
+            get { return Enum.GetValues(typeof(MouseButtons)).Cast<MouseButtons>().ToList(); }
+        }
+        
         private PointsSources pointSource;
         public PointsSources PointsSource
         {
             get { return pointSource; }
             set { SetProperty(ref pointSource, value); }
+        }
+        
+        private TimeSpan pointsMousePositionSampleInterval;
+        public TimeSpan PointsMousePositionSampleInterval
+        {
+            get { return pointsMousePositionSampleInterval; }
+            set { SetProperty(ref pointsMousePositionSampleInterval, value); }
+        }
+        
+        private TimeSpan pointTtl;
+        public TimeSpan PointTtl
+        {
+            get { return pointTtl; }
+            set { SetProperty(ref pointTtl, value); }
+        }
+        
+        private TriggerSources keySelectionTriggerSource;
+        public TriggerSources KeySelectionTriggerSource
+        {
+            get { return keySelectionTriggerSource; }
+            set { SetProperty(ref keySelectionTriggerSource, value); }
+        }
+        
+        private TriggerSources pointSelectionTriggerSource;
+        public TriggerSources PointSelectionTriggerSource
+        {
+            get { return pointSelectionTriggerSource; }
+            set { SetProperty(ref pointSelectionTriggerSource, value); }
+        }
+        
+        private Keys selectionTriggerKeyboardKeyDownUpKey;
+        public Keys SelectionTriggerKeyboardKeyDownUpKey
+        {
+            get { return selectionTriggerKeyboardKeyDownUpKey; }
+            set { SetProperty(ref selectionTriggerKeyboardKeyDownUpKey, value); }
+        }
+        
+        private MouseButtons selectionTriggerMouseDownUpButton;
+        public MouseButtons SelectionTriggerMouseDownUpButton
+        {
+            get { return selectionTriggerMouseDownUpButton; }
+            set { SetProperty(ref selectionTriggerMouseDownUpButton, value); }
+        }
+        
+        private TimeSpan pointSelectionTriggerFixationLockOnTime;
+        public TimeSpan PointSelectionTriggerFixationLockOnTime
+        {
+            get { return pointSelectionTriggerFixationLockOnTime; }
+            set { SetProperty(ref pointSelectionTriggerFixationLockOnTime, value); }
+        }
+        
+        private TimeSpan pointSelectionTriggerFixationCompleteTime;
+        public TimeSpan PointSelectionTriggerFixationCompleteTime
+        {
+            get { return pointSelectionTriggerFixationCompleteTime; }
+            set { SetProperty(ref pointSelectionTriggerFixationCompleteTime, value); }
+        }
+        
+        private double pointSelectionTriggerFixationRadius;
+        public double PointSelectionTriggerFixationRadius
+        {
+            get { return pointSelectionTriggerFixationRadius; }
+            set { SetProperty(ref pointSelectionTriggerFixationRadius, value); }
+        }
+        
+        private TimeSpan keySelectionTriggerFixationLockOnTime;
+        public TimeSpan KeySelectionTriggerFixationLockOnTime
+        {
+            get { return keySelectionTriggerFixationLockOnTime; }
+            set { SetProperty(ref keySelectionTriggerFixationLockOnTime, value); }
+        }
+        
+        private TimeSpan keySelectionTriggerFixationCompleteTime;
+        public TimeSpan KeySelectionTriggerFixationCompleteTime
+        {
+            get { return keySelectionTriggerFixationCompleteTime; }
+            set { SetProperty(ref keySelectionTriggerFixationCompleteTime, value); }
         }
 
         public bool ChangesRequireRestart
@@ -79,11 +179,33 @@ namespace JuliusSweetland.ETTA.UI.ViewModels.Management
         private void LoadSettings()
         {
             PointsSource = Settings.Default.PointsSource;
+            PointsMousePositionSampleInterval = Settings.Default.PointsMousePositionSampleInterval;
+            PointTtl = Settings.Default.PointTtl;
+            KeySelectionTriggerSource = Settings.Default.KeySelectionTriggerSource;
+            PointSelectionTriggerSource = Settings.Default.PointSelectionTriggerSource;
+            SelectionTriggerKeyboardKeyDownUpKey = Settings.Default.SelectionTriggerKeyboardKeyDownUpKey;
+            SelectionTriggerMouseDownUpButton = Settings.Default.SelectionTriggerMouseDownUpButton;
+            PointSelectionTriggerFixationLockOnTime = Settings.Default.PointSelectionTriggerFixationLockOnTime;
+            PointSelectionTriggerFixationCompleteTime = Settings.Default.PointSelectionTriggerFixationCompleteTime;
+            PointSelectionTriggerFixationRadius = Settings.Default.PointSelectionTriggerFixationRadius;
+            KeySelectionTriggerFixationLockOnTime = Settings.Default.KeySelectionTriggerFixationLockOnTime;
+            KeySelectionTriggerFixationCompleteTime = Settings.Default.KeySelectionTriggerFixationCompleteTime;
         }
 
         public void ApplyChanges()
         {
             Settings.Default.PointsSource = PointsSource;
+            Settings.Default.PointsMousePositionSampleInterval = PointsMousePositionSampleInterval;
+            Settings.Default.PointTtl = PointTtl;
+            Settings.Default.KeySelectionTriggerSource = KeySelectionTriggerSource;
+            Settings.Default.PointSelectionTriggerSource = PointSelectionTriggerSource;
+            Settings.Default.SelectionTriggerKeyboardKeyDownUpKey = SelectionTriggerKeyboardKeyDownUpKey;
+            Settings.Default.SelectionTriggerMouseDownUpButton = SelectionTriggerMouseDownUpButton;
+            Settings.Default.PointSelectionTriggerFixationLockOnTime = PointSelectionTriggerFixationLockOnTime;
+            Settings.Default.PointSelectionTriggerFixationCompleteTime = PointSelectionTriggerFixationCompleteTime;
+            Settings.Default.PointSelectionTriggerFixationRadius = PointSelectionTriggerFixationRadius;
+            Settings.Default.KeySelectionTriggerFixationLockOnTime = KeySelectionTriggerFixationLockOnTime;
+            Settings.Default.KeySelectionTriggerFixationCompleteTime = KeySelectionTriggerFixationCompleteTime;
             Settings.Default.Save();
         }
 
