557b6a9f925a7b36381ecbddc66d30aafa41ee32
Julius <juliussweetland@gmail.com>
2015-02-26 14:00:16 +0000

    Update CursorHost.cs

--- src/UI/Controls/CursorHost.cs
+++ src/UI/Controls/CursorHost.cs
@@ -6,7 +6,6 @@ using System.Windows.Forms;
 using JuliusSweetland.OptiKey.Extensions;
 using JuliusSweetland.OptiKey.Properties;
 using JuliusSweetland.OptiKey.Static;
-using log4net;
 
 namespace JuliusSweetland.OptiKey.UI.Controls
 {
@@ -14,12 +13,13 @@ namespace JuliusSweetland.OptiKey.UI.Controls
     {
         #region Private member vars
 
-        private readonly static ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-
         private Window window;
         private Screen screen;
+        private Point point = new Point(0,0);
+        private bool selectionInProgress;
         private Point screenTopLeft;
         private Point screenBottomRight;
+        private Point screenBottomRightInWpfCoords;
 
         #endregion
 
@@ -28,6 +28,61 @@ namespace JuliusSweetland.OptiKey.UI.Controls
         public CursorHost()
         {
             Loaded += OnLoaded;
+        }
+
+        #endregion
+        
+        #region On Loaded
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            window = Window.GetWindow(this);
+            Screen = window.GetScreen();
+            
+            var mainViewModel = DataContext as MainViewModel;
+            
+            //IsOpen
+            Action<KeyValue?> calculateIsOpen = value => value == SelectionModes.Point;
+            mainViewModel.OnPropertyChanges(vm => vm.SelectionMode).Subscribe(calculateIsOpen);
+            calculateIsOpen();
+            
+            //Calculate position based on CurrentPositionPoint
+            mainViewModel.OnPropertyChanges(vm => vm.CurrentPositionPoint)
+                .Where(cpp => cpp != null && !selectionInProgress) //Only set current Point if we are not within a selection/fixation
+                .Subscribe(cpp => Point = cpp.Value);
+            
+            //Calculate position based on PointSelectionProgress
+            mainViewModel.OnPropertyChanges(vm => vm.PointSelectionProgress)
+                .Subscribe(psp => 
+                {
+                    if(psp == null)
+                    {
+                        //Selection/fixation not in progress
+                        selectionInProgress = false;
+                    }
+                    else
+                    {
+                        //Selection/fixation in progress
+                        selectionInProgress = true;
+                        Point = psp.Item1;
+                    }
+                });
+            
+            //Subscribe to window location changes and re-evaluate the current screen and current position
+            Observable.FromEventPattern<EventHandler, EventArgs>
+                (h => window.LocationChanged += h,
+                 h => window.LocationChanged -= h)
+                .Throttle(TimeSpan.FromSeconds(0.1))
+                .ObserveOnDispatcher()
+                .Subscribe(_ =>
+                {
+                    Log.Debug("Window's LocationChanged event detected.");
+                    Screen = window.GetScreen();
+                });
+
+            //Re-calculate cursor host position on size changes
+            this.OnPropertyChanges<double>(ActualWidthProperty).Subscribe(_ => CalculatePosition());
+            this.OnPropertyChanges<double>(ActualHeightProperty).Subscribe(_ => CalculatePosition());
         }
 
         #endregion
@@ -43,96 +98,93 @@ namespace JuliusSweetland.OptiKey.UI.Controls
                 {
                     screen = value;
 
-                    if (value != null)
-                    {
-                        screenTopLeft = new Point(screen.Bounds.Left, screen.Bounds.Top);
-                        screenBottomRight = new Point(screen.Bounds.Right, screen.Bounds.Bottom);
-                        CalculatePosition();
-                    }
+                    screenTopLeft = new Point(screen.Bounds.Left, screen.Bounds.Top);
+                    screenBottomRight = new Point(screen.Bounds.Right, screen.Bounds.Bottom);
+                    screenBottomRightInWpfCoords = window.GetTransformFromDevice().Transform(screenBottomRight);
+                    
+                    CalculatePosition();
+                }
+            }
+        }
+        
+        //N.B. The sources 'CurrentPositionPoint' and 'PointSelectionProgress.Item1' are in the device co-ordinate system, so Point is too
+        private Point Point
+        {
+            get { return point; }
+            set
+            {
+                if(point != value)
+                {
+                    point = value;
+                    CalculatePosition();
                 }
             }
         }
 
-        //SelectionMode DP bound to MVM.SelectionMode
-        //    -> IsOpen = SelectionMode == SelectionModes.Point;
+        public static readonly DependencyProperty IsPivottedHorizontallyProperty =
+            DependencyProperty.Register("IsPivottedHorizontally", typeof(bool), typeof(CursorHost), new PropertyMetadata(default(bool)));
 
-        //React to IsOpen changes - CalculatePoint();
-
-        //CurrentPoint DP bound to MVM.CurrentPositionPoint
-        //    -> if(PointSelectionProgress == null)
-        //        {
-        //            CalculatePosition();
-        //        }
-
-        //SelectionProgress DP bound to MVM.PointSelectionProgress
-        //    -> CalculatePosition();
-
-        //IsPivottedHorizontally
-        //IsPivottedVertically
-
-        #endregion
-
-        #region On Loaded
-
-        private void OnLoaded(object sender, RoutedEventArgs e)
+        public bool IsPivottedHorizontally
         {
-            window = Window.GetWindow(this);
-            Screen = window.GetScreen();
-            CalculatePosition();
+            get { return (bool)GetValue(IsPivottedHorizontallyProperty); }
+            set { SetValue(IsPivottedHorizontallyProperty, value); }
+        }
 
-            //Subscribe to window location changes and re-evaluate the current screen
-            Observable.FromEventPattern<EventHandler, EventArgs>
-                (h => window.LocationChanged += h,
-                 h => window.LocationChanged -= h)
-                .Throttle(TimeSpan.FromSeconds(0.1))
-                .ObserveOnDispatcher()
-                .Subscribe(_ =>
-                {
-                    Log.Debug("Window's LocationChanged event detected.");
-                    Screen = window.GetScreen();
-                    CalculatePosition();
-                });
+        public static readonly DependencyProperty IsPivottedVerticallyProperty =
+            DependencyProperty.Register("IsPivottedVertically", typeof(bool), typeof(CursorHost), new PropertyMetadata(default(bool)));
 
-            //Re-calculate cursor host position on size changes
-            this.OnPropertyChanges<double>(ActualWidthProperty).Subscribe(_ => CalculatePosition());
-            this.OnPropertyChanges<double>(ActualHeightProperty).Subscribe(_ => CalculatePosition());
+        public bool IsPivottedVertically
+        {
+            get { return (bool)GetValue(IsPivottedVerticallyProperty); }
+            set { SetValue(IsPivottedVerticallyProperty, value); }
         }
 
         #endregion
 
         #region Calculate Position
 
-        //N.B. CurrentPoint and SelectionProgress.Point are in the device co-ordinate system
-        //N.B. Offsets appear to be in DPI, i.e. WPF coordinate system
         private void CalculatePosition()
         {
-            //if (IsOpen
-            //    && Screen != null)
-            //{
-            //    Point point = SelectionProgress != null
-            //        ? SelectionProgress.Point
-            //        : CurrentPoint;
+            //Copy point locally as point is not threadsafe
+            var pointCopy = Point;
+            
+            //Popup is open and point is within screen bounds
+            if (IsOpen
+                && pointCopy.X >= screenTopLeft.X
+                && pointCopy.X <= screenBottomRight.X
+                && pointCopy.Y >= screenTopLeft.Y
+                && pointCopy.Y <= screenBottomRight.Y)
+            {
+                //N.B. Offsets are in the WPF coordinate system, so we now need to compare everything in that system
+                //DIPs = pixels / (DPI/96.0) from https://msdn.microsoft.com/en-us/library/windows/desktop/dd371316(v=vs.85).aspx / https://msdn.microsoft.com/en-us/library/windows/desktop/ff684173(v=vs.85).aspx
+                var dpiPoint = new Point((pointCopy.X / (Graphics.DpiX / 96)), (pointCopy.Y / (Graphics.DpiY / 96)));
 
-            //    if (point != null)
-            //    {
-            //        //Check if point is within screen bounds
-            //        if (point.X >= screenTopLeft.X
-            //            && point.X <= screenBottomRight.X
-            //            && point.Y >= screenTopLeft.Y
-            //            && point.Y <= screenBottomRight.Y)
-            //        {
-            //            //Convert point to DPI points - https://msdn.microsoft.com/en-us/library/windows/desktop/ff684173(v=vs.85).aspx
-            //            //DIPs = pixels / (DPI/96.0) from https://msdn.microsoft.com/en-us/library/windows/desktop/dd371316(v=vs.85).aspx
-            //            var dpiPoint = new Point((point.X / (Graphics.DpiX / 96)), (point.Y / (Graphics.DpiY / 96)));
-
-                        //Adjust offsets for screen boundaries vs ActualWidth & ActualHeight
-                        //Set HorizontalPosition and VerticalPosition
-
-            //            HorizontalOffset = dpiPoint.X;
-            //            VerticalOffset = dpiPoint.Y;
-            //        }
-            //    }
-            //}
+                //Coerce horizontal offset
+                var horizontalAdjustmentAmount = 0d;
+                if(dpiPoint.X + ActualWidth > screenBottomRightInWpfCoords.X)
+                {
+                    horizontalAdjustmentAmount = 0 - ActualWidth;
+                    IsPivottedHorizontally = true;
+                }
+                else
+                {
+                    IsPivottedHorizontally = false;
+                }
+                HorizontalOffset = dpiPoint.X + horizontalAdjustmentAmount;
+                
+                //Coerce vertical offset
+                var verticalAdjustmentAmount = 0d;
+                if(dpiPoint.Y + ActualHeight > screenBottomRightInWpfCoords.Y)
+                {
+                    verticalAdjustmentAmount = 0 - ActualHeight;
+                    IsPivottedVertically = true;
+                }
+                else
+                {
+                    IsPivottedVertically = false;
+                }
+                VerticalOffset = dpiPoint.Y + verticalAdjustmentAmount;
+            }
         }
 
         #endregion
