94e9856b93a2a9bcaf541894bbfe204bb230032a
Julius <juliussweetland@gmail.com>
2015-03-12 18:54:09 +0000

    Fix singular/plural representation on pixel adjustment and scroll keys - CURRENTLY BROKEN

---
+++ src/UI/ValueConverters/IntToSingularPluralStringFormatter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace JuliusSweetland.OptiKey.UI.ValueConverters
+{
+    public class IntToSingularPluralStringFormatter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valueAsInt = (int)value;
+            var stringParam = parameter as string;
+            var splitParams = stringParam.Split('|');
+
+            return valueAsInt == 1
+                ? string.Format(splitParams[0], value)
+                : string.Format(splitParams[1], value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
