36a63dba57698c7e3f27bfa38fea57eb6838dcca
Julius <juliussweetland@gmail.com>
2015-03-12 18:54:09 +0000

    Fix singular/plural representation on pixel adjustment and scroll keys - CURRENTLY BROKEN

---
+++ src/Models/LastMouseActionStateManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Practices.Prism.Mvvm;
+
+namespace JuliusSweetland.OptiKey.Models
+{
+    public class LastMouseActionStateManager : BindableBase, ILastMouseActionStateManager
+    {
+        private Action lastMouseAction;
+        private bool lastMouseActionExists;
+
+        public Action LastMouseAction
+        {
+            get { return lastMouseAction; }
+            set
+            {
+                lastMouseAction = value;
+                LastMouseActionExists = lastMouseAction != null;
+            }
+        }
+
+        public bool LastMouseActionExists
+        {
+            get { return lastMouseActionExists; }
+            private set { SetProperty(ref lastMouseActionExists, value); }
+        }
+    }
+}
