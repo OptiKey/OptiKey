5fb6c29db040e5219c760a86541013fe0956c2f6
Julius <juliussweetland@gmail.com>
2015-08-07 08:19:26 +0100

    Adding WidthGreaterThanHeight value converter

---
+++ src/UI/ValueConverters/WidthGreaterThanHeight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace JuliusSweetland.OptiKey.UI.ValueConverters
+{
+    public class WidthGreaterThanHeight : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var frameworkElement = value as FrameworkElement;
+            if (frameworkElement != null)
+            {
+                return frameworkElement.ActualWidth >= frameworkElement.ActualHeight;
+            }
+
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
