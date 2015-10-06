ff384fcea6963e17329093e12a4a9c9356ae9a83
ckulesa <chad.kulesa@verificationsinc.com>
2015-10-06 15:45:13 -0500

    Created unit test to reproduce issue #88

---
+++ src/JuliusSweetland.OptiKey.UnitTests/Services/KeyboardOutputServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using JuliusSweetland.OptiKey.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuliusSweetland.OptiKey.UnitTests.Services
+{
+    [TestFixture]
+    public class KeyboardOutputServiceTests
+    {
+        private static readonly string dzSingleCodePoint = "\u01F3";
+        private static readonly string dzMultiCodePoint = "\u0064\u007A";
+
+        private static readonly string singleCodePointString = "12" + dzSingleCodePoint + "34";
+        private static readonly string multiCodePointString = "12" + dzMultiCodePoint +"34";
+
+        [Test]
+        public void SuggestionSingleCodePointToMultiCodePoint()
+        {
+            //Arrange
+            var mockKeyStateService = new Mock<IKeyStateService>();
+            mockKeyStateService.Setup(x => x.KeyDownStates).Returns(new Models.NotifyingConcurrentDictionary<Models.KeyValue, Enums.KeyDownStates>());
+
+            var mockSuggestionService = new Mock<ISuggestionStateService>();
+            mockSuggestionService.Setup(x => x.Suggestions).Returns(new List<string> { singleCodePointString, multiCodePointString });
+
+            var mockPublishService = new Mock<IPublishService>();
+
+            var mockDictionaryService = new Mock<IDictionaryService>();
+
+            var keyboardOutputService = new KeyboardOutputService(mockKeyStateService.Object, mockSuggestionService.Object, mockPublishService.Object, mockDictionaryService.Object, x => { });
+
+            //Act
+            keyboardOutputService.ProcessSingleKeyText("1");
+            keyboardOutputService.ProcessFunctionKey(Enums.FunctionKeys.Suggestion1);
+            keyboardOutputService.ProcessFunctionKey(Enums.FunctionKeys.Suggestion2);
+
+            //Assert
+            Assert.AreEqual(multiCodePointString, keyboardOutputService.Text);
+        }
+
+        [Test]
+        public void SuggestionMultiCodePointToSingleCodePoint()
+        {
+            //Arrange
+            var mockKeyStateService = new Mock<IKeyStateService>();
+            mockKeyStateService.Setup(x => x.KeyDownStates).Returns(new Models.NotifyingConcurrentDictionary<Models.KeyValue, Enums.KeyDownStates>());
+
+            var mockSuggestionService = new Mock<ISuggestionStateService>();
+            mockSuggestionService.Setup(x => x.Suggestions).Returns(new List<string> { singleCodePointString, multiCodePointString });
+
+            var mockPublishService = new Mock<IPublishService>();
+
+            var mockDictionaryService = new Mock<IDictionaryService>();
+
+            var keyboardOutputService = new KeyboardOutputService(mockKeyStateService.Object, mockSuggestionService.Object, mockPublishService.Object, mockDictionaryService.Object, x => { });
+
+            //Act
+            keyboardOutputService.ProcessSingleKeyText("1");
+            keyboardOutputService.ProcessFunctionKey(Enums.FunctionKeys.Suggestion2);
+            keyboardOutputService.ProcessFunctionKey(Enums.FunctionKeys.Suggestion1);
+
+            //Assert
+            Assert.AreEqual(singleCodePointString, keyboardOutputService.Text);
+        }
+    }
+}
