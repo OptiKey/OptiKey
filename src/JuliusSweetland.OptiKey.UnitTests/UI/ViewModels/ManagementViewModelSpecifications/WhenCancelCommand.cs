2bc9dfe7bd7aac80f161a1c19def968c0b510925
Dan Padmore <dan.padmore@telenet.be>
2015-10-28 21:38:36 +0100

    Capture behavior of cancel on ManagementViewModel

---
+++ src/JuliusSweetland.OptiKey.UnitTests/UI/ViewModels/ManagementViewModelSpecifications/WhenCancelCommand.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Windows;
+
+namespace JuliusSweetland.OptiKey.UnitTests.UI.ViewModels.ManagementViewModelSpecifications
+{
+    [TestFixture]
+    public class WhenCancelCommand : ManagementViewModelTestBase
+    {
+        protected Window Window { get; private set; }
+        protected bool IsWindowClosed { get; private set; }
+
+        protected override void Arrange()
+        {
+            base.Arrange();
+            Window = new Window();
+            Window.Closed += (s, e) => IsWindowClosed = true;
+        }
+
+        protected override void Act()
+        {
+            ManagementViewModel.CancelCommand.Execute(Window);
+        }
+
+        [RequiresSTA]
+        [Test]
+        public void ThenWindowShouldBeClosed()
+        {
+            Assert.IsTrue(IsWindowClosed);
+        }
+    }
+}
