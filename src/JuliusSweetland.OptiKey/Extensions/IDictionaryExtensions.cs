bc032adbd2b12ce1d1b40ddac334464c723c3963
Julius <juliussweetland@gmail.com>
2015-12-20 13:51:20 +0000

    Adds new IDictionary extension methods to return default value if key not found

---
+++ src/JuliusSweetland.OptiKey/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JuliusSweetland.OptiKey.Extensions
+{
+    public static class IDictionaryExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue)
+        {
+            TValue value;
+            return dictionary.TryGetValue(key, out value) ? value : defaultValue;
+        }
+
+        public static TValue GetValueOrDefault<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> defaultValueProvider)
+        {
+            TValue value;
+            return dictionary.TryGetValue(key, out value) ? value : defaultValueProvider();
+        }
+    }
+}
