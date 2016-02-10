996ddece5004600d8e938e5867fafa3083c58ec5
andylamp <andreas.grammenos@gmail.com>
2016-02-10 18:41:16 +0200

    First push to the Greek translation branch
    -- Added a new language in the Languages enumeration
    -- Fully implented Standard, Conversation keyboards
    -- Added required actions in KeyboardHost, KeyValues, FunctionKeys
    -- Updated default .resx file to have the Greek (required when selecting a language)

--- src/JuliusSweetland.OptiKey/Enums/Languages.cs
+++ src/JuliusSweetland.OptiKey/Enums/Languages.cs
@@ -5,12 +5,11 @@ namespace JuliusSweetland.OptiKey.Enums
 {
     public enum Languages
     {
-	    DutchBelgium,
-        DutchNetherlands,
         EnglishCanada,
         EnglishUK,
         EnglishUS,
-	    FrenchFrance,
+        FrenchFrance,
+        GreekGreece,
         GermanGermany,
         RussianRussia
     }
@@ -21,13 +20,12 @@ namespace JuliusSweetland.OptiKey.Enums
         {
             switch (languages)
             {
-                case Languages.DutchBelgium: return Resources.DUTCH_BELGIUM;
-                case Languages.DutchNetherlands: return Resources.DUTCH_NETHERLANDS;
                 case Languages.EnglishCanada: return Resources.ENGLISH_CANADA;
                 case Languages.EnglishUK: return Resources.ENGLISH_UK;
                 case Languages.EnglishUS: return Resources.ENGLISH_US;
                 case Languages.FrenchFrance: return Resources.FRENCH_FRANCE;
                 case Languages.GermanGermany: return Resources.GERMAN_GERMANY;
+                case Languages.GreekGreece: return Resources.GREEK_GREECE;
                 case Languages.RussianRussia: return Resources.RUSSIAN_RUSSIA;
             }
 
@@ -38,13 +36,12 @@ namespace JuliusSweetland.OptiKey.Enums
         {
             switch (languages)
             {
-                case Languages.DutchBelgium: return CultureInfo.GetCultureInfo("nl-BE");
-                case Languages.DutchNetherlands: return CultureInfo.GetCultureInfo("nl-NL");			
                 case Languages.EnglishUS: return CultureInfo.GetCultureInfo("en-US");
                 case Languages.EnglishUK: return CultureInfo.GetCultureInfo("en-GB");
                 case Languages.EnglishCanada: return CultureInfo.GetCultureInfo("en-CA");
                 case Languages.FrenchFrance: return CultureInfo.GetCultureInfo("fr-FR");
                 case Languages.GermanGermany: return CultureInfo.GetCultureInfo("de-DE");
+                case Languages.GreekGreece: return CultureInfo.GetCultureInfo("el-GR");
                 case Languages.RussianRussia: return CultureInfo.GetCultureInfo("ru-RU");
             }
 
