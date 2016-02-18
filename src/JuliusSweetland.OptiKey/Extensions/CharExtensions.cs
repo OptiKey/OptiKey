a0fc7af78c38cbf865883ecfb5bad6ca1ee5d2dd
Julius <juliussweetland@gmail.com>
2016-02-18 22:00:12 +0000

    Adds IsCombiningCharacter extension method

--- src/JuliusSweetland.OptiKey/Extensions/CharExtensions.cs
+++ src/JuliusSweetland.OptiKey/Extensions/CharExtensions.cs
@@ -54,6 +54,14 @@ namespace JuliusSweetland.OptiKey.Extensions
                 || category == UnicodeCategory.EnclosingMark; //Enclosing mark character, which is a nonspacing combining character that surrounds all previous characters up to and including a base character. Signified by the Unicode designation "Me" (mark, enclosing). The value is 7.
         }
 
+        public static bool IsCombiningCharacter(this char c)
+        {
+            var category = CharUnicodeInfo.GetUnicodeCategory(c);
+            return category == UnicodeCategory.NonSpacingMark //(All combining diacritic characters are non-spacing marks). Nonspacing character that indicates modifications of a base character. Signified by the Unicode designation "Mn"(mark, nonspacing).The value is 5.
+                || category == UnicodeCategory.SpacingCombiningMark //Spacing character that indicates modifications of a base character and affects the width of the glyph for that base character. Signified by the Unicode designation "Mc" (mark, spacing combining). The value is 6.
+                || category == UnicodeCategory.EnclosingMark; //Enclosing mark character, which is a nonspacing combining character that surrounds all previous characters up to and including a base character. Signified by the Unicode designation "Me" (mark, enclosing). The value is 7.
+        }
+
         //http://inputsimulator.codeplex.com/SourceControl/latest#WindowsInput/Native/VirtualKeyCode.cs
         //http://msdn.microsoft.com/en-gb/library/windows/desktop/dd375731(v=vs.85).aspx
         public static VirtualKeyCode? ToVirtualKeyCode(this char character)
