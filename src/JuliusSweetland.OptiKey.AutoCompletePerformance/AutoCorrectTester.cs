4d8eb0e7316e597d738a406410d14f75933142f9
Edd Porter <optikey@eddporter.com>
2016-02-25 07:32:36 +0000

    Create IAutoComplete testing harness

---
+++ src/JuliusSweetland.OptiKey.AutoCompletePerformance/AutoCorrectTester.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using JuliusSweetland.OptiKey.Enums;
+
+namespace JuliusSweetland.OptiKey.AutoCompletePerformance
+{
+    public class AutoCorrectTestResults
+    {
+        public AutoCorrectTestResults(AutoCompleteMethods autoCompleteMethod,
+            IEnumerable<SpellingCorrectionTestResult> results, long memorySize)
+        {
+            AutoCompleteMethod = autoCompleteMethod;
+            Results = results;
+            MemorySize = memorySize;
+        }
+
+        public AutoCompleteMethods AutoCompleteMethod { get; private set; }
+        public long MemorySize { get; private set; }
+        public IEnumerable<SpellingCorrectionTestResult> Results { get; private set; }
+    }
+
+    internal class AutoCorrectTester
+    {
+        private const string SpellingErrorsFile = "misspellings{0}.csv";
+        private readonly Languages language;
+        private IEnumerable<MisspellingTest> misspellings;
+
+        public AutoCorrectTester(Languages language)
+        {
+            this.language = language;
+        }
+
+        public IEnumerable<AutoCorrectTestResults> Run()
+        {
+            LoadMisspellings();
+
+            var results = new List<AutoCorrectTestResults>();
+
+            foreach (AutoCompleteMethods autoCompleteMethod in Enum.GetValues(typeof (AutoCompleteMethods)))
+            {
+                var before = GC.GetTotalMemory(true);
+
+                var tester = new SpellingCorrectionTester(autoCompleteMethod, language);
+                var testResults = misspellings.Select(tester.TestWord);
+
+                var after = GC.GetTotalMemory(false);
+
+                results.Add(new AutoCorrectTestResults(autoCompleteMethod, testResults, after - before));
+            }
+
+            return results;
+        }
+
+        private void LoadMisspellings()
+        {
+            var source =
+                File.ReadAllLines(string.Format(SpellingErrorsFile, language)).Skip(1).Select(line => line.Split(','));
+            misspellings = source.Select(strings => new MisspellingTest(strings[0].Trim(), strings[1].Trim()));
+        }
+    }
+}
---
+++ src/JuliusSweetland.OptiKey.AutoCompletePerformance/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JuliusSweetland.OptiKey.AutoCompletePerformance")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JuliusSweetland.OptiKey")]
+[assembly: AssemblyCopyright("Licensed under the terms of the GNU GPL v3")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyFileVersion("1.0.0.0")]
