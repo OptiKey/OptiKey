37548c948783a242762339dd796b8c1f543978fb
Kirsty McNaught <kirsty_mcn@yahoo.com>
2017-09-09 18:38:27 +0100

    Remove unused converter

--- src/JuliusSweetland.OptiKey/Models/ChangeKeyboardKeyValue.cs
+++ src/JuliusSweetland.OptiKey/Models/ChangeKeyboardKeyValue.cs
@@ -12,7 +12,6 @@ using JuliusSweetland.OptiKey.Extensions;
 
 namespace JuliusSweetland.OptiKey.Models
 {
-    [TypeConverter(typeof(KeyValueLinkConverter))]
     public class ChangeKeyboardKeyValue : KeyValue, IEquatable<ChangeKeyboardKeyValue>
     {
 
@@ -133,43 +132,6 @@ namespace JuliusSweetland.OptiKey.Models
             stringBuilder.Append(this.replaceKeyboard);
             
             return stringBuilder.ToString();
-        }
-    }
-
-    public sealed class KeyValueLinkConverter : TypeConverter
-    {
-        // The ITypeDescriptorContext interface provides the context for the
-        // conversion. Typically, this interface is used at design time to 
-        // provide information about the design-time container.
-        public override bool CanConvertFrom(ITypeDescriptorContext context,
-           Type sourceType)
-        {
-            if (sourceType == typeof(string))
-            {
-                return true;
-            }
-            return base.CanConvertFrom(context, sourceType);
-        }
-
-        public override object ConvertFrom(ITypeDescriptorContext context,
-           CultureInfo culture, object value)
-        {
-            var text = value as string;
-            if (text!=null)
-            {
-                return new ChangeKeyboardKeyValue(text);
-            }
-            return base.ConvertFrom(context, culture, value);
-        }
-
-        public override object ConvertTo(ITypeDescriptorContext context,
-           CultureInfo culture, object value, Type destinationType)
-        {
-            if (destinationType == typeof(string))
-            {
-                return ((ChangeKeyboardKeyValue)value).String;
-            }
-            return base.ConvertTo(context, culture, value, destinationType);
         }
     }
 }
