8bc0818e4b32b34052668be1e0d4c137fae05456
Julius Sweetland <juliussweetland@gmail.com>
2018-02-21 18:11:07 +0000

    Fixes incorrect settings value

--- src/JuliusSweetland.OptiKey/Properties/Settings.Designer.cs
+++ src/JuliusSweetland.OptiKey/Properties/Settings.Designer.cs
@@ -1764,8 +1764,7 @@ namespace JuliusSweetland.OptiKey.Properties {
             "  <value>\r\n      <ticks>PT1.75S</ticks>\r\n    </value>\r\n  </item>\r\n  <item>\r\n    " +
             "<key>\r\n      <keyValue>\r\n        <functionKey>ItalianItaly</functionKey>\r\n      " +
             "</keyValue>\r\n    </key>\r\n    <value>\r\n      <ticks>PT1.75S</ticks>\r\n    </value>" +
-            "\r\n  </item>\r\n  <item>\r\n    <key>\r\n      <keyValue />\r\n    </key>\r\n    <value>\r\n " +
-            "     <ticks>PT1.75S</ticks>\r\n    </value>\r\n  </item>\r\n  <item>\r\n    <key>\r\n     " +
+            "\r\n  </item>\r\n  <item>\r\n    <key>\r\n     " +
             " <keyValue>\r\n        <functionKey>JapaneseJapan</functionKey>\r\n      </keyValue>\r\n" +
             "    </key>\r\n    <value>\r\n      <ticks>PT1.75S</ticks>\r\n    </value>\r\n  </item>\r\n  <item>\r\n    <key>\r\n     " +
             " <keyValue>\r\n        <functionKey>KoreanKorea</functionKey>\r\n      </keyValue>\r\n" +
