80982a9000833b88c63fcaa7e640e3d87e0fdd22
zelmon64 <zelmon64@gmail.com>
2018-05-15 20:53:57 +0100

    Show button borders and dynamicly set animation colours
    The OBF button border definitions are now used to set a border around the
    keys. The corner tags that indicate special keys now use the border
    colour insead of black. This boarder is slightly thicker than the hover
    border so that the two can be seen simultaneously.
    
    The hover border colour and the key selection progress animation now use
    either black or white depending on the border and background colours.

---
+++ src/JuliusSweetland.OptiKey/UI/ValueConverters/TwoColoursToForegroundBrush.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using JuliusSweetland.OptiKey.Properties;
+
+namespace JuliusSweetland.OptiKey.UI.ValueConverters
+{
+    public class TwoColoursToForegroundBrush : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            Color Border = (Color)ColorConverter.ConvertFromString(values[0].ToString());
+            int colourSum = Border.R + Border.G + Border.B;
+            //*
+            if (Border.A < 0xFF)
+            {
+                Color Background = (Color)ColorConverter.ConvertFromString(values[1].ToString());
+                int colourSumBackground = Background.R + Background.G + Background.B;
+                if (Background.A < 0xFF)
+                {
+                    if (Settings.Default.Theme.ToLower().Contains("light"))
+                        colourSumBackground = (colourSumBackground * Background.A + (0xFF - Background.A) * 0xFF * 3) / 0xFF;
+                    else
+                        colourSumBackground = (colourSumBackground * Background.A) / 0xFF;
+                }
+                colourSum = (colourSum * Border.A + (0xFF - Border.A) * colourSumBackground) / 0xFF;
+            }
+            //*/
+            if (colourSum < 0x17E) // 0XFF * 3 / 2
+                return new SolidColorBrush(Colors.White);
+            else
+                return new SolidColorBrush(Colors.Black);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
