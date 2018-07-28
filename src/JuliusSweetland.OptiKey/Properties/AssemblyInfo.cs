5ae776848a8ce4f45634cd75c60bd1c7a93befea
Julius Sweetland <juliussweetland@gmail.com>
2018-07-28 08:47:55 +0100

    Changes version to 2.17.0

--- src/JuliusSweetland.OptiKey/Properties/AssemblyInfo.cs
+++ src/JuliusSweetland.OptiKey/Properties/AssemblyInfo.cs
@@ -60,7 +60,7 @@ using System.Windows;
 //MAJOR version when you make incompatible API changes,
 //MINOR version when you add functionality in a backwards-compatible manner, and
 //PATCH version when you make backwards-compatible bug fixes (in the Version class this is the Build number).
-[assembly: AssemblyVersion("2.16.0")]
+[assembly: AssemblyVersion("2.17.0")]
 
 [assembly: InternalsVisibleTo("JuliusSweetland.OptiKey.UnitTests")]
 [assembly: InternalsVisibleTo("JuliusSweetland.OptiKey.AutoCompletePerformance")]
