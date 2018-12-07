a3e2311a4c68bb1f4da19b88e2f84e6ae2371917
baljakbratislav <batistuta13b@gmail.com>
2018-12-07 18:08:45 +0100

    Add serbian keyboard

---
+++ src/JuliusSweetland.OptiKey/UI/Views/Keyboards/Serbian/Alpha1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using JuliusSweetland.OptiKey.UI.Controls;
+
+
+namespace JuliusSweetland.OptiKey.UI.Views.Keyboards.Serbian
+{
+    /// <summary>
+    /// Interaction logic for Alpha1.xaml
+    /// </summary>
+    public partial class Alpha1 : KeyboardView
+    {
+        public Alpha1()
+        {
+            InitializeComponent();
+        }
+    }
+}
