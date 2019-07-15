66c8d02170f2de338b1dfbcf7189985505db0153
K McNaught <kirsty.mcnaught@gmail.com>
2019-07-15 14:53:10 +0100

    Add basic "Symbol" app stub

---
+++ src/JuliusSweetland.OptiKey.Symbol/Properties/Settings.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2019 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
+
+namespace JuliusSweetland.OptiKey.Symbol.Properties
+{
+
+    class Settings : JuliusSweetland.OptiKey.Properties.Settings
+    {
+
+        public static void Initialise()
+        {
+            Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+            InitialiseWithDerivedSettings(defaultInstance);
+        }
+
+        // Example of an overridden default
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Bold")]
+        [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
+        public new string FontWeight
+        {
+            get
+            {
+                return ((string)(this["FontWeight"]));
+            }
+            set
+            {
+                this["FontWeight"] = value;
+            }
+        }
+
+        // Example of an extra config for exe
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
+        public bool ExtraThing
+        {
+            get
+            {
+                return ((bool)(this["ExtraThing"]));
+            }
+            set
+            {
+                this["ExtraThing"] = value;
+            }
+        }
+
+    }
+}
