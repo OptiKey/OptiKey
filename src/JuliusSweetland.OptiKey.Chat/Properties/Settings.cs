ba21d37aa9ffd87538a493bbceb96184848b7079
K McNaught <kirsty.mcnaught@gmail.com>
2019-07-16 12:29:00 +0100

    update settings for conversation app

--- src/JuliusSweetland.OptiKey.Chat/Properties/Settings.cs
+++ src/JuliusSweetland.OptiKey.Chat/Properties/Settings.cs
@@ -13,59 +13,9 @@ namespace JuliusSweetland.OptiKey.Chat.Properties
         }
 
         /*
-         * Override all the settings relating to Communikate keyboard setup, to lock user into
-         * Communikate conversation mode only.
+         * Override the settings relating to conversation mode, to lock user into
+         * conversation mode only.
          */
-
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
-        public override bool EnableCommuniKateKeyboardLayout
-        {
-            get
-            {
-                return true;
-            }
-            set
-            {
-                // no-op
-            }
-        }
-
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
-        public override bool UseCommuniKateKeyboardLayoutByDefault
-        {
-            get
-            {
-                return true;
-            }
-            set
-            {
-                // no-op
-            }
-        }
-
-
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
-        public override bool UsingCommuniKateKeyboardLayout
-        {
-            get
-            {
-                return true;
-            }
-            set
-            {
-                // no-op
-            }
-        }
-
 
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -79,7 +29,7 @@ namespace JuliusSweetland.OptiKey.Chat.Properties
             }
             set
             {
-                // no-op
+                // no-op, can't be changed in this app
             }
         }
 
