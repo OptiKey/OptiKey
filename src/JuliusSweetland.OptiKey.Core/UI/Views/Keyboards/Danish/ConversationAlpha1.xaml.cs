bfed7a4c8a0e235cae1b99cb2a1a7598f9e19d10
Julius Sweetland <juliussweetland@gmail.com>
2019-07-18 22:06:21 +0100

    Restores lost Danish keyboard. Moves new files into Optikey.Core

--- src/JuliusSweetland.OptiKey.Core/UI/Views/Keyboards/Danish/ConversationAlpha1.xaml.cs
+++ src/JuliusSweetland.OptiKey.Core/UI/Views/Keyboards/Danish/ConversationAlpha1.xaml.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Copyright (c) 2019 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
+using JuliusSweetland.OptiKey.UI.Controls;
+
+namespace JuliusSweetland.OptiKey.UI.Views.Keyboards.Danish
+{
+    /// <summary>
+    /// Interaction logic for ConversationAlpha1.xaml
+    /// </summary>
+    public partial class ConversationAlpha1 : KeyboardView
+    {
+        public ConversationAlpha1() : base(shiftAware: true)
+        {
+            InitializeComponent();
+        }
+    }
+}
