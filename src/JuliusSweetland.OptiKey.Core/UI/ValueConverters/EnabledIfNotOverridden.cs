7e59ba65f4ed380e010412121c151f4146b26f51
K McNaught <kirsty.mcnaught@gmail.com>
2019-08-19 13:08:35 +0100

    Add converter for disabling overridden properties

---
+++ src/JuliusSweetland.OptiKey.Core/UI/ValueConverters/EnabledIfNotOverridden.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2019 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using JuliusSweetland.OptiKey.Properties;
+
+namespace JuliusSweetland.OptiKey.UI.ValueConverters
+{
+    public class EnabledIfNotOverridden: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string propName = value?.ToString();
+            return !Settings.Default.IsOverridden(propName);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
