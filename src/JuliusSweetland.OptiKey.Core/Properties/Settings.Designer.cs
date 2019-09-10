df1951232d93c6a5712a5c4dd2447f21542adfa3
AdamRoden <adam.roden@gmail.com>
2019-09-10 16:12:39 -0500

    Added resource for MagnifyCenterOnCursor

--- src/JuliusSweetland.OptiKey.Core/Properties/Settings.Designer.cs
+++ src/JuliusSweetland.OptiKey.Core/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace JuliusSweetland.OptiKey.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -2645,6 +2645,19 @@ namespace JuliusSweetland.OptiKey.Properties {
             }
             set {
                 this["KeySelectionTriggerFixationCompleteTimesByKeyValues"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
+        public string MagnifyCenterOnCursor {
+            get {
+                return ((string)(this["MagnifyCenterOnCursor"]));
+            }
+            set {
+                this["MagnifyCenterOnCursor"] = value;
             }
         }
     }
