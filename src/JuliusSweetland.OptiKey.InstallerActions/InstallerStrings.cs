dc5676e833f8b8d04c13c3d15769800f234e688f
K McNaught <kirsty.mcnaught@gmail.com>
2019-11-25 10:15:38 +0000

    Update comment whitespace

--- src/JuliusSweetland.OptiKey.InstallerActions/InstallerStrings.cs
+++ src/JuliusSweetland.OptiKey.InstallerActions/InstallerStrings.cs
@@ -11,7 +11,9 @@ namespace JuliusSweetland
         // These all get automatically initialised by running the output executable of the InstallerTranslations project
         // which gets built and run as a dependency of InstallerActions.
         // Please don't commit changes to this file. To avoid changes showing up in your git index, run:
+        //
         // git update-index --skip-worktree InstallerStrings.cs
+        //
         public static Dictionary<CultureInfo, string> ALIENWARE_17_INFO;
 	    public static Dictionary<CultureInfo, string> GAZE_TRACKER_INFO;
 	    public static Dictionary<CultureInfo, string> IRISBOND_DUO_INFO;
