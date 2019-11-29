a5de8a2881677745cc33dda990eb2dbf3d1e620d
Julius Sweetland <juliussweetland@gmail.com>
2019-11-29 17:07:09 +0000

    Fixes absolute references to app.manifest file from each separate application

--- installer/4.Cleanup cache directories and prevent git updating installer strings.bat
+++
@@ -1,8 +0,0 @@
-rd /s /q "OptiKeyChat-cache"
-rd /s /q "OptiKeyMouse-cache"
-rd /s /q "OptiKeyPro-cache"
-rd /s /q "OptiKeySymbol-cache"
-pause
-
-git update-index --skip-worktree ../src/JuliusSweetland.OptiKey.InstallerActions/InstallerStrings.cs
-pause
