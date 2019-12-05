d29061a1ba18d1837ab364b8390e475afbc9730e
Julius Sweetland <juliussweetland@gmail.com>
2019-12-05 11:25:44 +0000

    Updates IrisBond assemblies, and corrects icons

--- installer/3.Cleanup cache directories and prevent git updating installer strings.bat
+++ installer/3.Cleanup cache directories and prevent git updating installer strings.bat
@@ -2,7 +2,6 @@ rd /s /q "OptiKeyChat-cache"
 rd /s /q "OptiKeyMouse-cache"
 rd /s /q "OptiKeyPro-cache"
 rd /s /q "OptiKeySymbol-cache"
-pause
 
 git update-index --skip-worktree ../src/JuliusSweetland.OptiKey.InstallerActions/InstallerStrings.cs
 pause
