1696b60df34277ecc07a16e73248bb4d96d849cb
Julius <juliussweetland@gmail.com>
2019-12-13 11:35:54 +0000

    Upgrade to 3.0.2, fixes logging and app versions

--- installer/3.Cleanup cache directories and prevent git updating installer strings.bat
+++ installer/3.Cleanup cache directories and prevent git updating installer strings.bat
@@ -3,5 +3,7 @@ rd /s /q "OptiKeyMouse-cache"
 rd /s /q "OptiKeyPro-cache"
 rd /s /q "OptiKeySymbol-cache"
 
+del /q "*.back.aip"
+
 git update-index --skip-worktree ../src/JuliusSweetland.OptiKey.InstallerActions/InstallerStrings.cs
 pause
