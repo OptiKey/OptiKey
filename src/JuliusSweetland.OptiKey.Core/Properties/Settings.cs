bd220f59d0b6e14ef2de2b8617abc0b0a679f068
K McNaught <kirsty.mcnaught@gmail.com>
2020-01-24 10:25:27 +0000

    Allow derived apps to report which app they are

--- src/JuliusSweetland.OptiKey.Core/Properties/Settings.cs
+++ src/JuliusSweetland.OptiKey.Core/Properties/Settings.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) 2019 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
 
+using JuliusSweetland.OptiKey.Enums;
 using log4net;
 
 namespace JuliusSweetland.OptiKey.Properties {
@@ -7,6 +8,9 @@ namespace JuliusSweetland.OptiKey.Properties {
     public abstract class Settings : global::System.Configuration.ApplicationSettingsBase {
 
         private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        // Derived classes must specify what app they are
+        public abstract AppType GetApp();
 
         private static Settings defaultInstance;
         
--- src/JuliusSweetland.OptiKey.Symbol/Properties/Settings.cs
+++ src/JuliusSweetland.OptiKey.Symbol/Properties/Settings.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) 2019 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
 
+using JuliusSweetland.OptiKey.Enums;
+
 namespace JuliusSweetland.OptiKey.Symbol.Properties
 {
 
@@ -10,6 +12,11 @@ namespace JuliusSweetland.OptiKey.Symbol.Properties
         {
             Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
             InitialiseWithDerivedSettings(defaultInstance);
+        }
+
+        public override AppType GetApp()
+        {
+            return AppType.Symbol;
         }
 
         /*
