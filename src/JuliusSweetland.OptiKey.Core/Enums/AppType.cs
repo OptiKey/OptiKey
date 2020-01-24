95fc6ae26be98c37671315345e5d609ff16f340a
K McNaught <kirsty.mcnaught@gmail.com>
2020-01-24 10:25:27 +0000

    Allow derived apps to report which app they are

---
+++ src/JuliusSweetland.OptiKey.Core/Enums/AppType.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2020 OPTIKEY LTD (UK company number 11854839) - All Rights Reserved
+namespace JuliusSweetland.OptiKey.Enums
+{
+    public enum AppType
+    {
+        Pro,
+        Mouse,
+        Chat,
+        Symbol
+    }
+}
