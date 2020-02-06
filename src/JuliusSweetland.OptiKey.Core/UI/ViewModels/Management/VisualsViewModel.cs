e3bb768e8d62b087ff5b3fbc9fe65d90e9f82abd
Julius <juliussweetland@gmail.com>
2020-02-06 13:43:17 +0000

    Revert "Add opacity to viewmodel"
    This reverts commit 8ee8887f41810970114333e39addbe3de435fedb.

--- src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Management/VisualsViewModel.cs
+++ src/JuliusSweetland.OptiKey.Core/UI/ViewModels/Management/VisualsViewModel.cs
@@ -557,13 +557,6 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels.Management
             set { SetProperty(ref mainWindowState, value); }
         }
 
-        private double mainWindowOpacity;
-        public double MainWindowOpacity
-        {
-            get { return mainWindowOpacity; }
-            set { SetProperty(ref mainWindowOpacity, value); }
-        }
-
         #endregion
 
         #region Methods
@@ -597,7 +590,6 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels.Management
             StartupKeyboardFile = Settings.Default.StartupKeyboardFile;
             DockPosition = Settings.Default.MainWindowDockPosition;
             MainWindowState = Settings.Default.MainWindowState;
-            MainWindowOpacity = Settings.Default.MainWindowOpacity;
         }
 
         public void ApplyChanges()
@@ -627,15 +619,11 @@ namespace JuliusSweetland.OptiKey.UI.ViewModels.Management
             Settings.Default.ConversationBorderThickness = ConversationBorderThickness;
             Settings.Default.DynamicKeyboardsLocation = DynamicKeyboardsLocation;
             Settings.Default.StartupKeyboardFile = StartupKeyboardFile;
-            
-            // Changes to window state, these methods will save the new values also
             if (Settings.Default.MainWindowState != MainWindowState || Settings.Default.MainWindowDockPosition != DockPosition)
             {
                 // this also saves the changes
                 windowManipulationService.ChangeState(MainWindowState, DockPosition);
             }
-            windowManipulationService.SetOpacity(MainWindowOpacity);
-
             // TODO: deal with currently-minimised state?? necessary for Optikey proper
         }
 
