29b83bec4074c9f691973147825a6ea8bdf80bc5
Sam <samuel.snowball@hotmail.co.uk>
2020-02-08 17:36:40 +0000

    Created translation plugin stub
    Created a new translation plugin class, and for debugging put it in the bighackerboard.xml file, this currently just loads notepad.exe.
    
    Not sure why TargetFrameworkVersion for SingleInstance has changed! Need to revert.

---
+++ src/JuliusSweetland.OptiKey.StandardPlugins/TranslationKey/TranslationKey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace JuliusSweetland.OptiKey.StandardPlugins
+{
+    public class TranslationKey
+    {
+        public void TRANSLATE(string text)
+        {
+            Process.Start(text);
+        }
+    }
+}
