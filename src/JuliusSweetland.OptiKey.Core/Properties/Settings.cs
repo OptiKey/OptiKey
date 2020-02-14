4a5b4e912bb57df0a310474e75aaeb7ab50afc6f
Sam <samuel.snowball@hotmail.co.uk>
2020-02-14 17:38:37 +0000

    Translation key sends request to yandex, logger config changes

--- src/JuliusSweetland.OptiKey.Core/Properties/Settings.cs
+++ src/JuliusSweetland.OptiKey.Core/Properties/Settings.cs
@@ -2691,7 +2691,7 @@ namespace JuliusSweetland.OptiKey.Properties {
 
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
         public virtual bool AllowMultipleInstances
         {
--- src/JuliusSweetland.OptiKey.StandardPlugins/TranslationKey/TranslationKey.cs
+++ src/JuliusSweetland.OptiKey.StandardPlugins/TranslationKey/TranslationKey.cs
@@ -5,13 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 
+using System.Net.Http;
+using System.IO;
+
 namespace JuliusSweetland.OptiKey.StandardPlugins
 {
     public class TranslationKey
     {
+        HttpClient client = new HttpClient();
+
         public void TRANSLATE(string text)
         {
-            Process.Start(text);
+            sendRequest();
         }
+
+        private async void sendRequest()
+        {
+            HttpResponseMessage response = await client.GetAsync(
+                "https://translate.yandex.net/api/v1.5/tr.json/translate?" +
+                "lang=de&" +
+                "key=trnsl.1.1.20200208T211513Z.c27ba519478a018a.0686d91c86a4321b32b41b0c30551aef47556314&" +
+                "text=Hello");
+            response.EnsureSuccessStatusCode();
+
+            //string responseCode = response.StatusCode.ToString();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Debug.Print("res is: " + responseBody);
+
+            Stream str = new FileStream(".", FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(str);
+            sw.WriteLine(responseBody);
+
+            Process.Start("notepad.exe", ".");
+        }
+      
     }
+
 }
