a7f6d0bab67435cea76ae7f5a93b19ca4969b6a7
Sam <samuel.snowball@hotmail.co.uk>
2020-03-03 15:05:27 +0000

    Reset development config

--- src/JuliusSweetland.OptiKey.Core/Properties/Settings.cs
+++ src/JuliusSweetland.OptiKey.Core/Properties/Settings.cs
@@ -2723,7 +2723,7 @@ namespace JuliusSweetland.OptiKey.Properties {
 
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         [global::System.Configuration.SettingsManageabilityAttribute(global::System.Configuration.SettingsManageability.Roaming)]
         public virtual bool AllowMultipleInstances
         {
