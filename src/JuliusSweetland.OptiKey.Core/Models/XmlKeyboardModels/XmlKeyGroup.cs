d543062cf0fd08fb5f81c3ad943a0465c7e8d0d8
AdamRoden <adam.roden@gmail.com>
2020-10-05 15:13:46 -0500

    Add custom border options to dynamic keyboards

--- src/JuliusSweetland.OptiKey.Core/Models/XmlKeyboardModels/XmlKeyGroup.cs
+++ src/JuliusSweetland.OptiKey.Core/Models/XmlKeyboardModels/XmlKeyGroup.cs
@@ -13,6 +13,9 @@ namespace JuliusSweetland.OptiKey.Models
         [XmlAttribute] public string BackgroundColor { get; set; }
         [XmlAttribute] public string KeyDisabledBackground { get; set; }
         [XmlAttribute] public string KeyDownBackground { get; set; }
+        [XmlAttribute] public string BorderColor { get; set; }
+        [XmlAttribute] public string BorderThickness { get; set; }
+        [XmlAttribute] public string CornerRadius { get; set; }
         [XmlAttribute] public string Opacity { get; set; }
         [XmlAttribute] public string KeyDisabledOpacity { get; set; }
         [XmlAttribute] public string KeyDownOpacity { get; set; }
