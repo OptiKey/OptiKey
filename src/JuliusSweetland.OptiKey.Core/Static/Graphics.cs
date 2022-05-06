3d1e6fd22c7abd18eee1b190c99b5b3aae3c5b84
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2022-05-06 12:07:07 +0100

    Cache DpiX/DpiY
    This is used by Tobii point source as well as many other
    places, and can be costly.

--- src/JuliusSweetland.OptiKey.Core/Static/Graphics.cs
+++ src/JuliusSweetland.OptiKey.Core/Static/Graphics.cs
@@ -3,44 +3,46 @@ using System;
 using System.Windows;
 using JuliusSweetland.OptiKey.Native;
 using JuliusSweetland.OptiKey.Native.Common.Enums;
+using Microsoft.Win32;
 
 namespace JuliusSweetland.OptiKey.Static
 {
     public static class Graphics
     {
-        public static int DpiX
+        static Graphics()
         {
-            get
+            DisplaySettingsChanged(); // Set initial values
+            SystemEvents.DisplaySettingsChanged += new EventHandler((s,e) => { DisplaySettingsChanged(); });
+        }
+
+        private static void DisplaySettingsChanged()
+        {
+            ComputeDpiXandY();
+        }
+
+        public static int DpiX { get; private set; }
+        public static int DpiY { get; private set; }
+
+        public static void ComputeDpiXandY()
+        {
+            using (var g = System.Drawing.Graphics.FromHwnd(IntPtr.Zero))
             {
-                using(var g = System.Drawing.Graphics.FromHwnd(IntPtr.Zero))
-                {
-                    var desktop = g.GetHdc();
-                    return PInvoke.GetDeviceCaps(desktop, (int)DeviceCap.LOGPIXELSX);
-                }
+                var desktop = g.GetHdc();
+                DpiX = PInvoke.GetDeviceCaps(desktop, (int)DeviceCap.LOGPIXELSX);
+                DpiY = PInvoke.GetDeviceCaps(desktop, (int)DeviceCap.LOGPIXELSY);
             }
         }
-        
+
         public static double DipScalingFactorX
         {
             get { return (double)DpiX / (double)96; }
-        }
-
-        public static int DpiY
-        {
-            get
-            {
-                using(var g = System.Drawing.Graphics.FromHwnd(IntPtr.Zero))
-                {
-                    var desktop = g.GetHdc();
-                    return PInvoke.GetDeviceCaps(desktop, (int)DeviceCap.LOGPIXELSY);
-                }
-            }
         }
         
         public static double DipScalingFactorY
         {
             get { return (double)DpiY / (double)96; }
         }
+
 
         public static double VirtualScreenWidthInPixels
         {
