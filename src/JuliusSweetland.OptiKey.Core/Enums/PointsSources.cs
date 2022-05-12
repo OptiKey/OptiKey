ffa05a1e2f1ab62deaab6053b856ffbb578894ee
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2022-05-12 09:10:35 +0100

    Obsolete Tobii gaming trackers

--- src/JuliusSweetland.OptiKey.Core/Enums/PointsSources.cs
+++ src/JuliusSweetland.OptiKey.Core/Enums/PointsSources.cs
@@ -4,23 +4,35 @@ namespace JuliusSweetland.OptiKey.Enums
 {
     public enum PointsSources
     {
+        [System.Obsolete("Not supported in v4+", true)]
         Alienware17,
+
         GazeTracker,
         IrisbondDuo,
         IrisbondHiru,
         MousePosition,
+
+        [System.Obsolete("Not supported in v4+", true)]
         SteelseriesSentry,
+
         TheEyeTribe,
+
+        [System.Obsolete("Not supported in v4+", true)]
         TobiiEyeTracker4C,
+        [System.Obsolete("Not supported in v4+", true)]
         TobiiEyeTracker5,
+        [System.Obsolete("Not supported in v4+", true)]
         TobiiEyeX,
+        [System.Obsolete("Not supported in v4+", true)]
         TobiiRex,
+
         TobiiPcEyeGo,
         TobiiPcEyeGoPlus,
         TobiiPcEye5,
         TobiiPcEyeMini,
         TobiiX2_30,
         TobiiX2_60,
+
         [System.Obsolete("Not supported in v4+", true)]
         VisualInteractionMyGaze
     }
@@ -31,17 +43,11 @@ namespace JuliusSweetland.OptiKey.Enums
         {
             switch (pointSource)
             {
-                case PointsSources.Alienware17: return Resources.ALIENWARE_17;
                 case PointsSources.GazeTracker: return Resources.GAZE_TRACKER;
                 case PointsSources.IrisbondDuo: return Resources.IRISBOND_DUO;
                 case PointsSources.IrisbondHiru: return Resources.IRISBOND_HIRU;
                 case PointsSources.MousePosition: return Resources.MOUSE_POSITION;
-                case PointsSources.SteelseriesSentry: return Resources.STEELSERIES_SENTRY;
                 case PointsSources.TheEyeTribe: return Resources.THE_EYE_TRIBE;
-                case PointsSources.TobiiEyeTracker4C: return Resources.TOBII_EYE_TRACKER_4C;
-                case PointsSources.TobiiEyeTracker5: return Resources.TOBII_EYE_TRACKER_5;
-                case PointsSources.TobiiEyeX: return Resources.TOBII_EYEX;
-                case PointsSources.TobiiRex: return Resources.TOBII_REX;
                 case PointsSources.TobiiPcEyeGo: return Resources.TOBII_PCEYE_GO;
                 case PointsSources.TobiiPcEyeGoPlus: return Resources.TOBII_PCEYE_GO_PLUS;
                 case PointsSources.TobiiPcEye5: return Resources.TOBII_PCEYE_5;
@@ -57,17 +63,11 @@ namespace JuliusSweetland.OptiKey.Enums
         {
             switch (pointSource)
             {
-                case PointsSources.Alienware17: return Resources.ALIENWARE_17_INFO;
                 case PointsSources.GazeTracker: return Resources.GAZE_TRACKER_INFO;
                 case PointsSources.IrisbondDuo: return Resources.IRISBOND_DUO_INFO;
                 case PointsSources.IrisbondHiru: return Resources.IRISBOND_HIRU_INFO;
                 case PointsSources.MousePosition: return Resources.MOUSE_POSITION_INFO;
-                case PointsSources.SteelseriesSentry: return Resources.TOBII_EYEX_INFO;
                 case PointsSources.TheEyeTribe: return "";
-                case PointsSources.TobiiEyeTracker4C: return Resources.TOBII_EYEX_INFO;
-                case PointsSources.TobiiEyeTracker5: return Resources.TOBII_EYEX_INFO;
-                case PointsSources.TobiiEyeX: return Resources.TOBII_EYEX_INFO;
-                case PointsSources.TobiiRex: return Resources.TOBII_ASSISTIVE_INFO;
                 case PointsSources.TobiiPcEyeGo: return Resources.TOBII_ASSISTIVE_INFO;
                 case PointsSources.TobiiPcEyeGoPlus: return Resources.TOBII_ASSISTIVE_INFO;
                 case PointsSources.TobiiPcEye5: return Resources.TOBII_ASSISTIVE_INFO;
