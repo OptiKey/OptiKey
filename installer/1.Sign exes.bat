e1dee6cd5acd8ea44a699d35d9506598ad516629
Julius Sweetland <juliussweetland@gmail.com>
2022-06-25 21:57:13 +0100

    Includes x64 files in Sign exe batch file (which is probably not required anyway)

--- installer/1.Sign exes.bat
+++ installer/1.Sign exes.bat
@@ -1,6 +1,11 @@
 "tools\signtool.exe" sign /n "Open Source Developer, Julius Sweetland" /t http://time.certum.pl /fd sha256 /v "..\src\JuliusSweetland.OptiKey.Chat\bin\Release\OptikeyChat.exe"
+"tools\signtool.exe" sign /n "Open Source Developer, Julius Sweetland" /t http://time.certum.pl /fd sha256 /v "..\src\JuliusSweetland.OptiKey.Chat\bin\x64\Release\OptikeyChat.exe"
 "tools\signtool.exe" sign /n "Open Source Developer, Julius Sweetland" /t http://time.certum.pl /fd sha256 /v "..\src\JuliusSweetland.OptiKey.Mouse\bin\Release\OptikeyMouse.exe"
+"tools\signtool.exe" sign /n "Open Source Developer, Julius Sweetland" /t http://time.certum.pl /fd sha256 /v "..\src\JuliusSweetland.OptiKey.Mouse\bin\x64\Release\OptikeyMouse.exe"
 "tools\signtool.exe" sign /n "Open Source Developer, Julius Sweetland" /t http://time.certum.pl /fd sha256 /v "..\src\JuliusSweetland.OptiKey.Pro\bin\Release\OptikeyPro.exe"
+"tools\signtool.exe" sign /n "Open Source Developer, Julius Sweetland" /t http://time.certum.pl /fd sha256 /v "..\src\JuliusSweetland.OptiKey.Pro\bin\x64\Release\OptikeyPro.exe"
 "tools\signtool.exe" sign /n "Open Source Developer, Julius Sweetland" /t http://time.certum.pl /fd sha256 /v "..\src\JuliusSweetland.OptiKey.Symbol\bin\Release\OptikeySymbol.exe"
+"tools\signtool.exe" sign /n "Open Source Developer, Julius Sweetland" /t http://time.certum.pl /fd sha256 /v "..\src\JuliusSweetland.OptiKey.Symbol\bin\x64\Release\OptikeySymbol.exe"
 "tools\signtool.exe" sign /n "Open Source Developer, Julius Sweetland" /t http://time.certum.pl /fd sha256 /v "..\src\TestControllers\bin\Release\TestControllers.exe"
+"tools\signtool.exe" sign /n "Open Source Developer, Julius Sweetland" /t http://time.certum.pl /fd sha256 /v "..\src\TestControllers\bin\x64\Release\TestControllers.exe"
 pause
