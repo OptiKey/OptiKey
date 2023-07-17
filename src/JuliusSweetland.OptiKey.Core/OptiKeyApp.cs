0f1443d4e13420b1639dfef19d0911acacc81e86
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2023-07-17 16:32:51 +0100

    remove tobii refs

--- src/JuliusSweetland.OptiKey.Core/OptiKeyApp.cs
+++ src/JuliusSweetland.OptiKey.Core/OptiKeyApp.cs
@@ -1010,9 +1010,8 @@ namespace JuliusSweetland.OptiKey
                             {
                                 if (currentVersion.Major < 4 && latestAvailableVersion.Major >= 4)
                                 {
-                                    //There should be no update prompt to upgrade from v3 (or earlier) to v4 (or later) due to a breaking change that means that v4 is not
-                                    //a suitable choice for many users on earlier version of Optikey (v4 removes supports for Tobii gaming devices).
-                                    Log.InfoFormat("An update is available, BUT this update could remove support for the user's current input device. The user will not be notified. " +
+                                    //There should be no update prompt to upgrade from v3 (or earlier) to v4 (or later) due to breaking changes
+                                    Log.InfoFormat("An update is available, but with major breaking changes. The user will not be notified. " +
                                                    "Current version is {0}. Latest version on GitHub repo is {1}", currentVersion, latestAvailableVersion);
                                 }
                                 else
