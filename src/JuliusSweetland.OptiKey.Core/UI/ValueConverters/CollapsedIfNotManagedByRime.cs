3d52d8a4d963aa59d9171dec9b4aadd054296df3
Kirsty McNaught <kirsty.mcnaught@gmail.com>
2023-08-14 11:47:16 +0100

    Don't show RIME options for non-chinese languages

---
+++ src/JuliusSweetland.OptiKey.Core/UI/ValueConverters/CollapsedIfNotManagedByRime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+using JuliusSweetland.OptiKey.Enums;
+using System.Windows;
+using System.Diagnostics;
+
+namespace JuliusSweetland.OptiKey.UI.ValueConverters
+{
+    public class CollapsedIfNotManagedByRime : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                if (Enum.TryParse(value.ToString(), out Languages language))
+                {
+                    if (language.ManagedByRime())
+                    {
+                        return Visibility.Visible;
+                    }
+                    else
+                    {
+                        return Visibility.Collapsed;
+                    }
+                }                
+            }
+
+            Debug.Assert(false, "Receiving an invalid language as input");
+            return Visibility.Collapsed;        
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
